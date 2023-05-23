7d2bd58337585406ce8eec4dec404461747534aa
HichamELMaa <hm.elmaataoui@gmail.com>
2023-05-23 07:27:34 -0700

    shell project

--- get_line.c
+++ get_line.c
@@ -1,29 +1,28 @@
 #include "shell.h"
 
 /**
- * get_line - Read a line from stdin.
+ * get_line - read a line from stdin
  *
- * Return: Pointer that points to a string with the line content.
+ * Return: pointer that points to a str with the line content
  */
 char *get_line(void)
 {
-    char *line = NULL;
-    size_t size = 0;
+	char *line = NULL;
+	size_t size = 0;
 
-    if (getline(&line, &size, stdin) == -1)
-    {
-        if (feof(stdin))
-        {
-            free(line);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            free(line);
-            perror("Error while reading the line from stdin");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    return (line);
+	if (getline(&line, &size, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			free(line);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			free(line);
+			perror("Error while reading the line from stdin");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
 }
