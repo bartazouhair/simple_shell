c5b46eccd5552bc007c6287ab77334b2669b0953
HichamELMaa <hm.elmaataoui@gmail.com>
2023-05-24 10:15:53 -0700

    Shell Project

--- my_own_exit.c
+++ my_own_exit.c
@@ -1,25 +1,19 @@
 #include "shell.h"
 
 /**
- * my_own_exit - Causes normal process termination.
- * @argm: Arguments.
+ * my_own_exit - couses normal process termination
+ * @argm: empty argm
  *
- * Return: 0 to terminate the process.
+ * Return: 0 to terminate the process
  */
 int my_own_exit(char **argm)
 {
-	int exit_status = 0;
-
-	if (argm[1] != NULL)
+	if (argm[1])
 	{
-		exit_status = atoi(argm[1]);
-		if (exit_status == 0 && strcmp(argm[1], "0") != 0)
-		{
-			fprintf(stderr, "Invalid exit status: %s\n", argm[1]);
-			return (-1);
-		}
+		return (atoi(argm[1]));
 	}
-
-	exit(exit_status);
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
